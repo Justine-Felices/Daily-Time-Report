41e7e74e93309b938e1001b9769198337af327e8
Justine-Felices <148616401+Justine-Felices@users.noreply.github.com>
2026-04-01 10:13:30 +0800

    change the middleware to proxy

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
-export async function middleware(request) {
+export async function proxy(request) {
   const { response, user } = await updateSession(request);
   const pathname = request.nextUrl.pathname;
 
